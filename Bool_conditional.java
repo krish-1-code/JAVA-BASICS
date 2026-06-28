6c4f716e51dcae04eb9856ae0cf5bd05534c1682
Krish-01-code <krishkushwaha.w@gmail.com>
2026-06-27 20:35:33 -0500

    Bool Conditions

---
+++ Bool_conditional.java
@@ -0,0 +1,16 @@
+public class Bool_conditional {
+    
+    public static void main(String[] args) {
+
+        boolean is_driving = true;
+
+        if(is_driving){
+            System.out.println("The person is driving");
+        }
+        else {
+            System.out.println("The person is not driving");
+        }
+
+
+    }
+}
