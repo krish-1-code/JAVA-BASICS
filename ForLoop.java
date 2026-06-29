4f5d08b363ff6e63711c53230ae25f409a5e456f
Krish-01-code <krishkushwaha.w@gmail.com>
2026-06-28 23:55:47 -0500

    For loop

---
+++ ForLoop.java
@@ -0,0 +1,18 @@
+public class ForLoop {
+    
+    public static void main(String[] args) {
+
+        int reps = 10;
+
+        for(int i = 1; i <= reps; i++){
+
+            System.out.println(i);
+        }
+
+        int[] numbers = {1,2,3,4,5};
+
+        for(int num : numbers){
+            System.out.print(num+"\t");
+        }
+    }
+}
