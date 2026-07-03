6e82fc327c340c8cda6e9d5474df1b7a5a9084dc
Krish-01-code <krishkushwaha.w@gmail.com>
2026-07-03 11:15:30 -0500

    Basic Mathematic operations

---
+++ Arithmetic.java
@@ -0,0 +1,26 @@
+public class Arithmetic {
+    public static void main(String[] args) {
+
+        //augmented assignment operations : += -= *= stuff like that;
+
+        //Order of operation = PEMDAS - parenthesis / exponents / multiplication / division / Addition / Substraction;
+
+        int a = 5 + 2;
+        System.out.println(a);
+
+        int b = a - 2;
+        System.out.println(b);
+
+        int c = a * b;
+        System.out.println(c);
+
+        double d = a / b;
+        System.out.println(d);
+
+        int x = c % a;
+        System.out.println(x);
+        
+        a += b; // example of augmented operations;
+        
+    }
+}
